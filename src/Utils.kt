56c6cbaf7b9763bcf0548cb265f373e2bc4e9254
Simeon L. <hi@simeon.lol>
2024-12-01 14:51:41 +0100

    feat: finished Day 01

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,6 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun List<String>.splitInput(): Pair<List<Int>, List<Int>> =
+    map { it.split("   ").let { (left, right) -> left.toInt() to right.toInt() } }.unzip()
